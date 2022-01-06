f5f79d261e97114d38872eb1e8da0798beb73681
florianGierlichs <gierlichs@gmx.de>
2022-01-06 14:16:03 +0100

    Add slash commands

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Replies with user info!"),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "9" }).setToken(BOT_TOKEN);
+
+rest
+  .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
--- index.js
+++ index.js
@@ -2,9 +2,32 @@ require("dotenv").config();
 
 const { Client, Intents } = require("discord.js");
 
-const TOKEN = process.env.BOT_TOKEN;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILD_SCHEDULED_EVENTS],
+  intents: [Intents.FLAGS.GUILDS],
 });
-client.login(TOKEN).then(console.log("läuft"));
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour username: ${interaction.user.username}`
+    );
+  }
+});
+
+client.login(BOT_TOKEN).then(console.log("client logged in"));
