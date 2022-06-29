ede4b963cdaa2c2d2371ba584a3bdf33da038957
florianGierlichs <gierlichs@gmx.de>
2022-06-29 15:44:00 +0200

    Fix typo

--- tests/convertTimestamp.test.ts
+++ tests/convertTimestamp.test.ts
@@ -2,11 +2,11 @@ import convertTimestamp from "../utils/convertTimestamp";
 
 jest.useFakeTimers().setSystemTime(new Date(2022, 0, 1, 12, 0, 0));
 
-const timezoneMock = new Date().getTime();
+const timestampMock = new Date().getTime();
 
 describe("convert timestamp", () => {
   it("should convert the timestamp to a specific date string", () => {
-    expect(convertTimestamp(timezoneMock)).toBe(
+    expect(convertTimestamp(timestampMock)).toBe(
       "Samstag, 1. Januar 2022, 12:00"
     );
   });
